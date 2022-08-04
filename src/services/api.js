980d99c4ca058425976f2b59e5d316c19fb65ef5
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-08-04 11:15:55 -0500

    update: test for deploy backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    // baseURL: "https://linkr-driven-api.herokuapp.com" //deploy
-    baseURL: "http://localhost:5000"
+    baseURL: "https://mauricio-mystudio.herokuapp.com" //deploy
+    // baseURL: "http://localhost:5000"
   });
 
   export const makeSignUp = async (formData) => {
